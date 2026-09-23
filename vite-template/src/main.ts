1bec979ee3dc5c27717ab2635ddf5ed728bc74fb
Johan Nordström <johan@digitalvillage.se>
2026-09-23 23:00:11 +0200

    Array-methods `.forEach()` and `.map()`.

--- vite-template/src/main.ts
+++ vite-template/src/main.ts
@@ -1,3 +1,29 @@
 import "./style.css";
 
-console.log("It works!");
+/**
+ * Array methods
+ */
+
+const fruits = ["apple", "banana", "pear"];
+
+const forEachRes = fruits.forEach((fruit) => {
+	console.log(fruit.toUpperCase());
+});
+console.log("forEachRes:", forEachRes);  // undefined
+console.log("fruits:", fruits);
+
+const mapRes = fruits.map((fruit) => {
+	return fruit.toUpperCase();
+});
+console.log("mapRes:", mapRes);  // [ "APPLE", "BANANA", "PEAR" ]
+console.log("fruits:", fruits);  // [ "apple", "banana", "pear" ]
+
+const fruitLengths = fruits.map((fruit) => {
+	return fruit.length;
+});
+console.log("fruitLengths:", fruitLengths);  // [5, 6, 4]
+
+const fruitLis = fruits.map(fruit => {
+	return `<li>${fruit}</li>`;
+});
+console.log("fruitLis:", fruitLis);
