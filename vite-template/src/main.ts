a327eacb20fa41a4a8fa90b1bc85b4d34f002fae
Johan Nordström <johan@digitalvillage.se>
2026-09-24 00:42:29 +0200

    Promises, `fetch` and `async`/`await`.

--- vite-template/src/main.ts
+++ vite-template/src/main.ts
@@ -1,3 +1,48 @@
 import "./style.css";
 
-console.log("It works!");
+/**
+ * Promises 🤞🏻
+ */
+
+/*
+console.log("Fetching a todo with a Promise...");
+
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+	.then(res => {
+		console.log("Got some response:", res);
+		if (!res.ok) {
+			throw new Error("Response was not ok 😭😭😭");
+		}
+
+		return res.json();
+	})
+	.then(data => {
+		console.log("NOW I CAN HAZ DATA", data);
+	})
+	.catch(err => {
+		console.error("Oh noez, broken promises 😳", err);
+	});
+
+*/
+
+// async/await
+const getTodos = async () => {
+	try {
+		console.log("Fetching a todo with async/await");
+
+		const res = await fetch("https://jsonplaceholder.typicode.com/todoszzzz/1");
+
+		if (!res.ok) {
+			throw new Error("Response was not ok 😭😭😭");
+		}
+
+		const data = await res.json();
+		console.log("NOW I CAN HAZ DATA", data);
+
+	} catch (err) {
+		console.error("Oh noez, error happened, but we caught it", err);
+	}
+
+}
+getTodos();
+console.log("Done?");
