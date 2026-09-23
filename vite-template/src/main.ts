c42b1b00d896cc6268709d64017a8cdc68a25bee
Johan Nordström <johan@digitalvillage.se>
2026-09-24 01:03:09 +0200

    Some quick TypeScript basics.

--- vite-template/src/main.ts
+++ vite-template/src/main.ts
@@ -1,3 +1,38 @@
 import "./style.css";
 
-console.log("It works!");
+/**
+ * TypeScript = Structurally Typed Type System
+ *
+ * Only type-check at build, **NOT** at runtime!
+ */
+
+let myString = "Johan";
+
+myString = "Pelle";
+// myString = 42;  // nope!
+
+let myNumber: number;
+myNumber = 1337;
+// myNumber = "LOLCATS!";  // also nope!
+
+
+const greet = (name: string, age: number) => {
+	console.log(`Hello ${name}! You are ${age.toFixed()} years old.`);
+}
+greet("Johan", 44);
+
+const add = (a: number, b: number, prefix?: string): string => {
+	const sum = a + b;
+
+	if (prefix) {
+		return prefix + " " + sum;
+	}
+
+	return sum.toString();
+
+	// return prefix
+	// 	? prefix + " " + sum
+	// 	: sum.toString();
+}
+console.log(add(3, 5));
+console.log(add(1332, 5, "Summa:"));
